47dd6e3d41d691c6f2350cef0fb4809f7ce0d255
marcorodnav <marcorodnav@gmail.com>
2018-03-24 15:13:15 -0600

    improved the sorting arr

--- src/main/java/awesome/challenge/implementation/FizzBuzzImpl.java
+++ src/main/java/awesome/challenge/implementation/FizzBuzzImpl.java
@@ -9,7 +9,30 @@ public class FizzBuzzImpl implements FizzBuzzable{
 
 	public String fizzBuzzMaker(int[] arr) {
 		// Make uncle bob proud...
-		return String.join(", ", Arrays.stream(arr).boxed().sorted().map(n->(n%5==0&&n%3==0?"FizzBuzz":(n%5==0?"Buzz":(n%3==0?"Fizz":String.valueOf(n))))).collect(Collectors.toList()));
-		
+		int[] sorted = new int[arr.length];
+		int min = arr[0];
+	    int max = arr[0];
+	    for (int i = 1; i < arr.length; i++) {
+	      if (arr[i] < min) {
+	        min = arr[i];
+	      } else if (arr[i] > max) {
+	        max = arr[i];
+	      }
+	    }
+	    int[] counts = new int[max - min + 1];
+	 
+	    for (int i = 0;  i < arr.length; i++) {
+	      counts[arr[i] - min]++;
+	    }
+	 
+	    counts[0]--;
+	    for (int i = 1; i < counts.length; i++) {
+	      counts[i] = counts[i] + counts[i-1];
+	    }
+	  
+	    for (int i = arr.length - 1; i >= 0; i--) {
+	    	sorted[counts[arr[i] - min]--] = arr[i];
+	    }
+		return Arrays.stream(sorted).boxed().map(n->(n%5==0&&n%3==0?"FizzBuzz":(n%5==0?"Buzz":(n%3==0?"Fizz":String.valueOf(n))))).collect(Collectors.joining(", "));
 	}
 }

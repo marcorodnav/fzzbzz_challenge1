150945f21b6e0ee9f7617c96c45ac783155ed3a6
marcorodnav <marcorodnav@gmail.com>
2018-03-24 12:27:01 -0600

    herpan solution

--- src/main/java/awesome/challenge/implementation/FizzBuzzImpl.java
+++ src/main/java/awesome/challenge/implementation/FizzBuzzImpl.java
@@ -1,11 +1,33 @@
 package awesome.challenge.implementation;
 
+import java.util.Arrays;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
+
 import awesome.challenge.definition.FizzBuzzable;
 
-public class FizzBuzzImpl implements FizzBuzzable{
+public class FizzBuzzImpl implements FizzBuzzable {
 
 	public String fizzBuzzMaker(int[] arr) {
 		// Make uncle bob proud...
-		return null;
+
+		IntFunction<String> MarcoJamasHubieraPensadoEnEsto = (int herpan) -> {
+			if (herpan % 15 == 0) {
+				return "FizzBuzz";
+			}
+			if (herpan % 3 == 0) {
+				return "Fizz";
+			}
+			if (herpan % 5 == 0) {
+				return "Buzz";
+			}
+			return String.valueOf(herpan);
+		};
+
+		String PalabrasSabiasDeMasterMarco = Arrays.stream(arr).boxed().sorted().map(
+				MarcoElCrackDeTodosLosTiempos -> MarcoJamasHubieraPensadoEnEsto.apply(MarcoElCrackDeTodosLosTiempos))
+				.collect(Collectors.joining(", "));
+
+		return PalabrasSabiasDeMasterMarco;
 	}
 }

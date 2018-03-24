8f3c278f58009e8a0408b4ae6433844e358a59a7
marcorodnav <marcorodnav@gmail.com>
2018-03-24 13:49:00 -0600

    fixed the solution

--- src/main/java/awesome/challenge/implementation/FizzBuzzImpl.java
+++ src/main/java/awesome/challenge/implementation/FizzBuzzImpl.java
@@ -1,6 +1,7 @@
 package awesome.challenge.implementation;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import awesome.challenge.definition.FizzBuzzable;
 
@@ -8,14 +9,7 @@ public class FizzBuzzImpl implements FizzBuzzable{
 
 	public String fizzBuzzMaker(int[] arr) {
 		// Make uncle bob proud...
-		Arrays.sort(arr);
-		String res = "";
-		for (int n=0;n<arr.length;n++) {
-			System.out.println(arr[n]);
-			res+= (arr[n]%5==0&&arr[n]%3==0?"FizzBuzz":(arr[n]%3==0?"Fizz":(arr[n]%5==0?"Buzz":String.valueOf(arr[n]))));
-			if (n != arr.length -1) res+= ", ";
-		}
-		return res;
+		return String.join(", ", Arrays.stream(arr).boxed().sorted().map(n->(n%5==0&&n%3==0?"FizzBuzz":(n%5==0?"Buzz":(n%3==0?"Fizz":String.valueOf(n))))).collect(Collectors.toList()));
 		
 	}
 }
